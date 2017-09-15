5843124e94d92bb5b4624945aab1bce21889274d
Sean B. Cleveland <sean.b.cleveland@gmail.com>
2017-09-14 14:05:17 -1000

    add the Agave configuration to the app.js
    add the Agave configuration to the app.js  and explicitly set the baser
    and tenant

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -36,9 +36,32 @@ var DsLandApp = angular
         redirectTo: '/'
       });
   });
-  DsLandApp.config(['$localStorageProvider',
-      function ($localStorageProvider) {
-          $localStorageProvider.get('token');
 
-          $localStorageProvider.set('token', 'ClientAppToken');
-      }]);
+angular.module('AgavePlatformScienceAPILib', ['ngStorage']).factory('Configuration', ['$localStorage', '$rootScope', '$timeout', function ($localStorage, $rootScope, $timeout) {
+
+      var Configuration = {
+          BASEURI: $localStorage.tenant ? $localStorage.tenant.baseUrl : 'https://agaveauth.its.hawaii.edu',
+          oAuthAccessToken: $localStorage.token ? $localStorage.token.access_token : '34dd97b2682e4ea2d9fe11676cf6c15',
+          setToken: function(token) {
+            this.oAuthAccessToken = token;
+          },
+          setBaseUri: function(baseUri) {
+            this.BASEURI = baseUri;
+          }
+      };
+
+      $rootScope.$watch('$localStorage.token', function(){
+          $timeout(function () {
+              Configuration.oAuthAccessToken = $localStorage.token ? $localStorage.token.access_token : '34dd97b2682e4ea2d9fe11676cf6c15';
+          }, 0);
+      }, true);
+
+      $rootScope.$watch('$localStorage.tenant', function(){
+          $timeout(function () {
+              Configuration.BASEURI = $localStorage.tenant ? $localStorage.tenant.baseUrl : 'hawaii';
+          }, 0);
+      }, true);
+
+      return Configuration;
+
+    }]);
